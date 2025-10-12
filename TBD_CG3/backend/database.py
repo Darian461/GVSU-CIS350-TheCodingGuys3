76df6506ba851b1efa8a2f739b425d80e74e1098
Jus1Dak <Dakota2steele@icloud.com>
2025-10-12 17:27:48 -0400

    Tuned logic for backend user reg/login, set up 2 apis with FastAPI in main, secured passwords and settings within settings, uploaded .env example to show you how to format, created JWT token authentication, linked code to PGadmin4 (If need help setting up let me know). Sorry for a lot of stuff in one PR wanted to get it done though.

--- TBD_CG3/backend/database.py
+++ TBD_CG3/backend/database.py
@@ -1,32 +1,19 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy_utils import database_exists, create_database
-from models import Base
-from settings import postgresql as settings
+from .settings import DATABASE_URL
 
+Base = declarative_base()
 
+# Create database if it doesn't exist
+if not database_exists(DATABASE_URL):
+    create_database(DATABASE_URL)
 
-
-def get_engine(user, passwd, host, port, db):
-    url = f"postgresql://{user}:{passwd}@{host}:{port}/{db}"
-    if not database_exists(url):
-        create_database(url)
-    engine = create_engine(url, pool_size=50, echo=False)
-    return engine
-
-# Initialize engine 
-engine = get_engine(
-    settings['pguser'],
-    settings['pgpassword'],
-    settings['pghost'],
-    settings['pgport'],
-    settings['pgdb']
-)
-
-Base.metadata.create_all(bind=engine)
-
-# Create a sessionmaker bound to the engine
+engine = create_engine(DATABASE_URL, pool_size=50, echo=False)
 SessionLocal = sessionmaker(bind=engine)
 
 def get_session():
     return SessionLocal()
+
+
+
